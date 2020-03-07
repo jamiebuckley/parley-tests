198d004cdeab63ac4a842dc9530cdfc0130f7547
jamie <jbuckley760@gmail.com>
2020-03-07 16:15:03 +0000

    Remove examples and add initial test for homepage

--- cypress/integration/parley/homePage.spec.js
+++ cypress/integration/parley/homePage.spec.js
@@ -3,8 +3,13 @@ context('Home Page', () => {
     cy.visit('/')
   })
 
+  it('displays the title in the navigation bar', () => {
+    cy.get('.logo').should('exist')
+    cy.get('.logo').contains('PARLEY')
+  })
+
   it('presents the available activities', () => {
-    cy.get('.create-retro').should('exist')
-    cy.get('.join-retro').should('exist')
+    cy.get('.host_meeting').should('exist')
+    cy.get('.join_meeting').should('exist')
   })
 })
